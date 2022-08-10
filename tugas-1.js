099480eed9efa279d5f71e0f63d7f09a5c5b90c7
Fahmi Idris <idrisfahmi253@gmail.com>
2022-08-10 21:12:32 +0700

    Update ; and change var to let

--- tugas-1.js
+++ tugas-1.js
@@ -1,35 +1,30 @@
-const ListA = [1,2,3,4,5]
-
-// variable untuk menampung array baru
-let results = []
+const ListA = [1,2,3,4,5];
 
 // Tidak menyertakan index di array baru
+let results = [];
+
 for (let index = 0; index < ListA.length; index++) {
-	let result = []
+  let result = [];
   
   ListA.forEach((listA, indexListA) => {
     if (index !== indexListA) {
-    	result.push(listA)
-    }
-  })
-  
-  results.push(result)
-}
+      result.push(listA);
+    };
+    results.push(result);
+  });
+};
 
 // Kalkulasi semua array baru
-let hasil = []
-
+let hasil = [];
 results.forEach((values) => {
-	let temp = 0
-  
+  let temp = 0;
   values.forEach((value) => {
-  	temp += value;
-  })
-  
-  hasil.push(temp)
-})
+    temp += value;
+  });
+  hasil.push(temp);
+});
 
 // Temukan nilai minimal dan maksimal
-var hasilMin = Math.min.apply(Math, hasil)
-var hasilMax = Math.max.apply(Math, hasil)
-console.log('Min : ', hasilMin, ',', 'Max : ',hasilMax)
+let hasilMin = Math.min.apply(Math, hasil);
+let hasilMax = Math.max.apply(Math, hasil);
+console.log('Min : ', hasilMin, ',', 'Max : ',hasilMax);
